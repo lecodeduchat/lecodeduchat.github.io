78262d78ef02d471c4550b29d5239137389ff0d2
lecodeduchat <lecodeduchat@gmail.com>
2022-07-22 08:51:36 +0200

    Modification du code js pour l'animation de la barre dégradée et animation des notes de musiques des deux chats dans hobby-music

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,49 @@
-// Détecter la taille d'écran
-let screenHeight = screen.height;
-console.log(screenHeight);
+// Animation de la barre dégradée
+const gradientColorBar = document.querySelector("#gradient-color-bar");
+const header = document.querySelector("header");
+const screenHeight = window.innerHeight;
 
-// Détecter le scroll d'une page
-
-window.onscroll = function () {
-  animateBar();
-};
-
-function animateBar() {
-  let scrollHeight = document.documentElement.scrollTop;
-  let elt = document.querySelector("#gradient-color-bar");
-  let header = document.querySelector("header");
-
-  if (scrollHeight > screenHeight) {
-    elt.style.top = "0px";
+window.addEventListener("scroll", () => {
+  if (window.scrollY > screenHeight) {
+    gradientColorBar.style.top = "0px";
     header.style.top = "4px";
   } else {
-    elt.style.top = "70px";
+    gradientColorBar.style.top = "70px";
     header.style.top = "0px";
   }
-}
-let tableMusicNotes = document.querySelectorAll("li.music-notes");
-function animateMusicNotes() {
-  let positionTopNotes = ["35px", "10px", "20px", "50px"];
-  let positionLeftNotes = ["90px", "120px", "150px", "60px"];
+});
 
-  for (let i = 0; i < 4; i++) {
-    tableMusicNotes[i].style.top = positionTopNotes[i];
-    tableMusicNotes[i].style.left = positionLeftNotes[i];
-  }
-  let elt = tableMusicNotes[3];
-  tableMusicNotes.unshift("+elt+");
-  tableMusicNotes.pop();
-  animateMusicNotes();
-}
-animateMusicNotes();
+//----------------------------------------------------------------------
+// Animation des chats qui chantent
+const musicHobby = document.getElementById("hobby-music");
+
+// Fabricateur de notes de musique
+const noteMaker = () => {
+  // cat left
+  const noteLeft = document.createElement("img");
+  noteLeft.classList.add("hobby-music");
+  noteLeft.classList.add("cat-left");
+  noteLeft.src = "/assets/svg/music-note.svg";
+  musicHobby.appendChild(noteLeft);
+
+  // cat right
+  const noteRight = document.createElement("img");
+  noteRight.classList.add("hobby-music");
+  noteRight.classList.add("cat-right");
+  noteRight.src = "/assets/svg/music-note.svg";
+  musicHobby.appendChild(noteRight);
+
+  // Modification de la variable css --left grâce à setProperty
+  noteLeft.style.setProperty("--left", Math.random() * 100 + "%");
+  noteRight.style.setProperty("--left", Math.random() * 100 + "%");
+
+  // On fait disparaître les notes une fois l'animation terminée, évite l'accumulation des notes hors de l'image
+  setTimeout(() => {
+    noteLeft.remove();
+    noteRight.remove();
+  }, 6000);
+};
+
+setInterval(noteMaker, 500);
+
+//----------------------------------------------------------------------
