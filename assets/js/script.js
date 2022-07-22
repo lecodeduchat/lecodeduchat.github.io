d0bbfcab3bbdae3d183f036d2193104ad626e69d
lecodeduchat <lecodeduchat@gmail.com>
2022-07-22 10:42:39 +0200

    Création de l'affichage aléatoire de notes de musique issue de arrayNotes

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,18 +19,28 @@ const musicHobby = document.getElementById("hobby-music");
 
 // Fabricateur de notes de musique
 const noteMaker = () => {
+  const arrayNotes = [
+    "/assets/svg/eighth-note.svg",
+    "/assets/svg/half-note.svg",
+    "/assets/svg/music-note.svg",
+    "/assets/svg/quarter-note.svg",
+    "/assets/svg/sixteenth-note.svg",
+  ];
+  // Création d'une variable aléatoire comprise entre 0 et 5 pour le choix d'une image issue du arrayNotes
+  const choice = Math.round(Math.random() * 10 * 0.5);
+
   // cat left
   const noteLeft = document.createElement("img");
   noteLeft.classList.add("hobby-music");
   noteLeft.classList.add("cat-left");
-  noteLeft.src = "/assets/svg/music-note.svg";
+  noteLeft.src = arrayNotes[choice];
   musicHobby.appendChild(noteLeft);
 
   // cat right
   const noteRight = document.createElement("img");
   noteRight.classList.add("hobby-music");
   noteRight.classList.add("cat-right");
-  noteRight.src = "/assets/svg/music-note.svg";
+  noteRight.src = arrayNotes[choice];
   musicHobby.appendChild(noteRight);
 
   // Modification de la variable css --left grâce à setProperty
