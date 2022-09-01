4880f005ac7fc4037350907dba00f8b06029f42a
lecodeduchat <lecodeduchat@gmail.com>
2022-09-01 12:33:56 +0200

    Configuration lecteur média btns next et shuffle

--- connivence/assets/js/player.js
+++ connivence/assets/js/player.js
@@ -29,14 +29,16 @@ const player = document.getElementById("player");
 const btnNext = document.getElementById("btn-next");
 const btnReadAmbiance = document.getElementById("btn-read-ambiance");
 const btnShuffleAmbiance = document.getElementById("btn-shuffle-ambiance");
+const btnReadAnimation = document.getElementById("btn-read-animation");
+const btnShuffleAnimation = document.getElementById("btn-shuffle-animation");
 const iconPause = '<img class="pause" src="/assets/svg/pause.svg" alt=""/>';
 const iconPlay = '<img class="play" src="/assets/svg/play.svg" alt=""/>';
 const buttons = document.querySelectorAll("button.btn-sm-music");
-let tracks = [];
+let tracksAndBtn = [];
 for (let i = 0; i < buttons.length; i++) {
   const idTrack = buttons[i].getAttribute("idTrack");
   const btnTrack = buttons[i];
-  tracks.push({ id: idTrack, btn: btnTrack });
+  tracksAndBtn.push({ id: idTrack, btn: btnTrack });
 }
 
 let btnMain = document.getElementById("btn-main");
@@ -51,44 +53,50 @@ let nextTrack = {
   button: buttons[1],
   src: playlistAmbiance[1].src,
 };
-let currentPlaylist = [];
+let currentPlaylist = arrayTracks;
+
+// FUNCTIONS ************************************************
 function pause(btnTrack) {
   player.pause();
   btnMain.innerHTML = btnTrack.innerHTML = iconPlay;
 }
 
 function play(idTrack) {
-  const track = tracks.find((elt) => elt.id == idTrack);
+  buttons.forEach((elt) => {
+    elt.innerHTML = iconPlay;
+  });
+  const track = tracksAndBtn.find((elt) => elt.id == idTrack);
   const btn = track.btn;
-  console.log(arrayTracks[idTrack].src);
-  player.setAttribute("src", arrayTracks[idTrack].src);
+  const piste = arrayTracks.find((elt) => elt.id == idTrack);
+  player.setAttribute("src", piste.src);
   player.play();
   btnMain.setAttribute("idTrack", idTrack);
   btnMain.innerHTML = btn.innerHTML = iconPause;
-  controlTitle.textContent = arrayTracks[idTrack].title;
+  controlTitle.textContent = piste.title;
   currentTrack = {
     id: idTrack,
     button: btn,
-    src: arrayTracks[idTrack].src,
+    src: piste.src,
   };
 }
 function next(idTrack, playlist) {
   for (let i = 0; i < playlist.length; i++) {
-    if (idTrack === playlist[i].idTrack) {
+    if (idTrack == playlist[i].id) {
       let j = i + 1;
-      if (i != playlist.lenght - 2) {
-        const idNextTrack = playlist[j].idTrack;
-        const btnNextTrack = buttons[idNextTrack];
+      if (i != playlist.length - 1) {
+        const idNextTrack = playlist[j].id;
+        const track = tracksAndBtn.find((elt) => elt.id == idNextTrack);
+        const btnNextTrack = track.btn;
         nextTrack = {
           id: idNextTrack,
           button: btnNextTrack,
-          src: arrayTracks[idNextTrack].src,
+          src: playlist[j].src,
         };
       } else {
         nextTrack = {
-          id: playlistAmbiance[1].id,
-          button: buttons[1],
-          src: playlistAmbiance[1].src,
+          id: playlist[0].id,
+          button: buttons[0],
+          src: playlist[0].src,
         };
       }
     }
@@ -112,18 +120,14 @@ closeControl.addEventListener("click", () => {
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener("click", () => {
     openControl();
+    const btnTrack = buttons[i];
     const idTrack = buttons[i].getAttribute("idTrack");
+    const piste = arrayTracks.find((elt) => elt.id == idTrack);
     if (player.paused) {
-      console.log(idTrack);
       play(idTrack);
-    } else if (currentTrack.src === arrayTracks[idTrack].src) {
-      console.log(currentTrack.src);
-      console.log(arrayTracks[idTrack].src);
+    } else if (currentTrack.src === piste.src) {
       pause(btnTrack);
     } else {
-      console.log("test");
-      console.log(currentTrack.src);
-      console.log(arrayTracks[idTrack].src);
       currentTrack.button.innerHTML = iconPlay;
       play(idTrack);
     }
@@ -137,13 +141,18 @@ btnMain.addEventListener("click", () => {
   }
 });
 btnNext.addEventListener("click", () => {
-  next(currentTrack.id, currentTrack.button);
+  next(currentTrack.id, currentPlaylist);
   play(nextTrack.id);
 });
 btnReadAmbiance.addEventListener("click", () => {
   openControl();
   currentPlaylist = playlistAmbiance;
   play(playlistAmbiance[0].id);
+});
+btnReadAnimation.addEventListener("click", () => {
+  openControl();
+  currentPlaylist = playlistAnimation;
+  play(playlistAnimation[0].id);
 });
 player.addEventListener("ended", () => {
   btnMain.innerHTML = currentBtnTrack.innerHTML = iconPlay;
@@ -152,7 +161,15 @@ player.addEventListener("ended", () => {
 });
 btnShuffleAmbiance.addEventListener("click", () => {
   openControl();
-  shuffle(playlistAmbiance);
+  currentPlaylist = arrayTracks.filter((elt) => elt.playlist === "ambiance");
+  shuffle(currentPlaylist);
+  const id = currentPlaylist[0].id;
+  play(id);
+});
+btnShuffleAnimation.addEventListener("click", () => {
+  openControl();
+  currentPlaylist = arrayTracks.filter((elt) => elt.playlist === "animation");
+  shuffle(currentPlaylist);
   const id = currentPlaylist[0].id;
   play(id);
 });
