1370b11a273c61627fbd787c82a63fde3164df1e
lecodeduchat <lecodeduchat@gmail.com>
2022-11-03 16:38:59 +0100

    ajout des border inclinées sur connivence

--- connivence/assets/js/script.js
+++ connivence/assets/js/script.js
@@ -8,3 +8,15 @@ btnMenu.addEventListener("click", () => {
 btnMenuClose.addEventListener("click", () => {
   menu.style.right = "-600px";
 });
+
+// Apparition du fond noir sous le header après scroll
+const header = document.querySelector("header");
+const screenHeight = 530;
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > screenHeight) {
+    header.style.backgroundColor = "black";
+  } else {
+    header.style.backgroundColor = "transparent";
+  }
+});
