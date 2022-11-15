a9f2e04de133fb262aca45c996a7ef7bcdbf3493
lecodeduchat <lecodeduchat@gmail.com>
2022-11-15 16:08:08 +0100

    Ajout des area avec boutons d'ajout

--- coolors/index.js
+++ coolors/index.js
@@ -1,8 +1,6 @@
 "use strict";
-const generate = document.getElementById("generate");
+const btnGenerate = document.getElementById("generate");
 const colors = document.querySelectorAll(".color");
-
-console.log(colors);
 
 function getRandomNumber(min, max) {
   let randomNumber = crypto.getRandomValues(new Uint32Array(1))[0];
@@ -16,18 +14,26 @@ function generateColor() {
   return color;
 }
 function newPalette() {
+  let palette = [];
   colors.forEach((color) => {
     let colorInfoName = color.querySelector(".color_info-name");
     let colorInfoHexa = color.querySelector(".color_info-hexa");
     let newColor = generateColor();
-    colorInfoName.textContent = newColor[0];
-    colorInfoHexa.textContent = newColor[1];
-    color.style.backgroundColor = newColor[1];
+    palette.push(newColor);
+    if (palette.length > 1) {
+      for (let i = 0; i < palette.length; i++) {
+        if (palette[i][0] != newColor[0]) {
+          colorInfoName.textContent = newColor[0];
+          colorInfoHexa.textContent = newColor[1];
+          color.style.backgroundColor = newColor[1];
+        }
+      }
+    }
   });
 }
 
 // Création aléatoire d'une nouvelle palette de couleurs au touch ou avec la barre d'espace
-generate.addEventListener("click", () => {
+btnGenerate.addEventListener("click", () => {
   newPalette();
 });
 document.addEventListener("keydown", function (event) {
